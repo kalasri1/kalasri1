8efbd697704d01fd7a80e394f885b34fbc07b350
Hi <Hi@DESKTOP-CR7BQJC.mshome.net>
2022-02-03 15:04:52 +0530

    This is my second Java program

---
+++ javalitetrals/src/javalitetrals/javaLiterals.java
@@ -0,0 +1,15 @@
+package javalitetrals;
+
+public class javaLiterals {
+
+	public static void main(String[] args) {
+		int learnerId=1312;
+		int learnerAge=24;
+		System.out.print("Learner Id:");
+		System.out.println(learnerId);
+		System.out.print("Learner Age:");
+		System.out.println(learnerAge);
+		
+	}
+
+}
